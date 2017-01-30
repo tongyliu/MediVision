205648792465aee5acbb8eca750967b7a50296d2
Han Zhang <han@zhanghan177.me>
2017-01-30 17:41:01 -0500

    Incorporate video.js player in website

--- web/client/app/js/main.js
+++ web/client/app/js/main.js
@@ -12,6 +12,14 @@ var App = React.createClass({
     return (
       <div className='app'>
         <h1>Hello, World!</h1>
+        <video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
+               poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+          <source src="/oceans.mp4" type='video/mp4'/>
+              <p class="vjs-no-js">
+                To view this video please enable JavaScript, and consider upgrading to a web browser that
+                <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+              </p>
+        </video>
       </div>
     );
   }
