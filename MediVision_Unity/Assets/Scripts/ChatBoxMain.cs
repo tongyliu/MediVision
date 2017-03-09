e91bc4f7362faeb140fbd8ef09c0e5c4733944e8
Chris Boston <bostonchris322@gmail.com>
2017-03-09 15:25:34 -0500

    Chat box skeleton
    caution, doesn't work yet

---
+++ MediVision_Unity/Assets/Scripts/ChatBoxMain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatBoxMain : MonoBehaviour
+{
+    ContentSizeFitter csf;
+    public Text msgText;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        csf = gameObject.GetComponent<ContentSizeFitter>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void showMessage(string msg)
+    {
+        msgText.text = msg;
+    }
+
+    //destroys gameobject after set amount of time
+    public void msgFader()
+    {
+
+    }
+
+
+
+
+
+}
---
+++ MediVision_Unity/Assets/Scripts/Message.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour
+{
+    public float fadeDelay = 20; //seconds
+    public float fadeSpeed = .01f;
+    public Text msgText;
+
+    float displayTime;
+    
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        displayTime = Time.time;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+        msgFader();
+    }
+
+    public void showMessage(string msg)
+    {
+        msgText.text = msg;
+    }
+
+    //destroys gameobject after set amount of time
+    public void msgFader()
+    {
+        if (Time.time - displayTime > fadeDelay && msgText.color.a > 0)
+        {
+            Color c = msgText.color;
+            c.a -= fadeSpeed;
+            msgText.color = c;
+            if (msgText.color.a < 0)
+            {
+                c.a = 0;
+                msgText.color = c;
+            }
+            if (msgText.color.a == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+}
