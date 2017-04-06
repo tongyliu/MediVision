1fc545dfd9cd8534e4a3d2f315874df9d8b29307
Tong Liu <tongyliu@gmail.com>
2017-04-06 16:42:39 -0400

    Create layout for login and register pages

--- web/client/app/js/components/register-page.js
+++ web/client/app/js/components/register-page.js
@@ -1,14 +1,72 @@
 var React = require('react');
 
 var RegisterPage = React.createClass({
+  getInitialState: function() {
+    return {
+      name: '',
+      username: '',
+      password: '',
+      retypedPassword: ''
+    };
+  },
+
   render: function() {
     return (
       <div className="register-page">
-        <div className="panel no-border">
-          <h1>Register</h1>
+        <h1>Sign Up</h1>
+        <div className="panel panel-default no-border">
+          <div className="panel-body">
+            <form onSubmit={this._handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="name-input">Name</label>
+                <input
+                  id="name-input" className="form-control" type="text"
+                  onChange={this._onChange.bind(this, 'name')}
+                  value={this.state.name}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="username-input">Username</label>
+                <input
+                  id="username-input" className="form-control" type="text"
+                  onChange={this._onChange.bind(this, 'username')}
+                  value={this.state.username}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password-input">Password</label>
+                <input
+                  id="password-input" className="form-control" type="password"
+                  onChange={this._onChange.bind(this, 'password')}
+                  value={this.state.password}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="confirm-input">Confirm Password</label>
+                <input
+                  id="confirm-input" className="form-control" type="password"
+                  onChange={this._onChange.bind(this, 'retypedPassword')}
+                  value={this.state.retypedPassword}
+                />
+              </div>
+              <button className="btn btn-lg btn-success">Go</button>
+            </form>
+          </div>
         </div>
       </div>
     );
+  },
+
+  _onChange: function(key, evt) {
+    var updateHash = {};
+    updateHash[key] = evt.target.value;
+    this.setState(updateHash);
+  },
+
+  _handleSubmit: function(evt) {
+    evt.preventDefault();
+    // TODO
+    console.log('register form submitted');
   }
 });
 
