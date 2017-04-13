ac2cc21a2732b1f99700796ac5e972a5ecc0d140
Tong Liu <tongyliu@gmail.com>
2017-04-13 18:02:27 -0400

    Finish implementing register page

--- web/client/app/js/components/register-page.js
+++ web/client/app/js/components/register-page.js
@@ -1,11 +1,14 @@
 /*
- * login-page.js
+ * register-page.js
  *
  * Page for users to sign up with new accounts
  */
 
 var React = require('react');
 var LoginManager = require('../login-manager');
+var ApiRequest = require('../utils/api-utils').ApiRequest;
+var classNames = require('classNames');
+var _ = require('lodash');
 
 var RegisterPage = React.createClass({
   getInitialState: function() {
@@ -13,14 +16,37 @@ var RegisterPage = React.createClass({
       name: '',
       username: '',
       password: '',
-      retypedPassword: ''
+      retypedPassword: '',
+      passwordsMatch: true,
+      isValid: false
     };
   },
 
+  componentWillMount: function() {
+    // Only check the password match status after typing has paused
+    this._checkPasswordsMatch = _.debounce(this._checkPasswordsMatch, 300);
+  },
+
   render: function() {
+    var registerFailedAlert = null;
+    if (this.state.registerFailed && this.state.errorDetail) {
+      registerFailedAlert = (
+        <div className="alert alert-danger" role="alert">
+          <strong>Whoops!</strong> Couldn't sign up: {this.state.errorDetail}
+        </div>
+      );
+    } else if (this.state.registerFailed) {
+      registerFailedAlert = (
+        <div className="alert alert-danger" role="alert">
+          <strong>Whoops!</strong> Failed to sign up, try again later.
+        </div>
+      );
+    }
+
     return (
       <div className="register-page">
         <h1>Sign Up</h1>
+        {registerFailedAlert}
         <div className="panel panel-default no-border">
           <div className="panel-body">
             <form onSubmit={this._handleSubmit}>
@@ -48,7 +74,9 @@ var RegisterPage = React.createClass({
                   value={this.state.password}
                 />
               </div>
-              <div className="form-group">
+              <div className={classNames('form-group', {
+                'has-error': !this.state.passwordsMatch
+              })}>
                 <label htmlFor="confirm-input">Confirm Password</label>
                 <input
                   id="confirm-input" className="form-control" type="password"
@@ -56,7 +84,11 @@ var RegisterPage = React.createClass({
                   value={this.state.retypedPassword}
                 />
               </div>
-              <button className="btn btn-lg btn-success">Go</button>
+              <button
+                className="btn btn-lg btn-success"
+                disabled={!this.state.isValid}>
+                Go
+              </button>
             </form>
           </div>
         </div>
@@ -73,13 +105,48 @@ var RegisterPage = React.createClass({
   _onChange: function(key, evt) {
     var updateHash = {};
     updateHash[key] = evt.target.value;
-    this.setState(updateHash);
+    if (key == 'password' || key == 'retypedPassword') {
+      this.setState(updateHash, this._checkPasswordsMatch);
+    } else {
+      this.setState(updateHash, this._validate);
+    }
+  },
+
+  _checkPasswordsMatch: function() {
+    if (this.state.retypedPassword) {
+      var isMatch = this.state.password == this.state.retypedPassword;
+      this.setState({ passwordsMatch: isMatch }, this._validate);
+    } else {
+      // Don't show an error if nothing has been typed yet
+      this.setState({ passwordsMatch: true }, this._validate);
+    }
+  },
+
+  _validate: function() {
+    var isValid = this.state.name && this.state.username && this.state.password
+                  && this.state.retypedPassword && this.state.passwordsMatch;
+    this.setState({ isValid: isValid });
   },
 
   _handleSubmit: function(evt) {
     evt.preventDefault();
-    // TODO
-    console.log('register form submitted');
+
+    new ApiRequest().post({ url: '/auth/create', form: {
+      name: this.state.name,
+      username: this.state.username,
+      password: this.state.password
+    }}, function(err, res, body) {
+      if (!err && res.statusCode == 200) {
+        if (body['success']) {
+          var userObj = { id: body['user_id'], name: this.state.name };
+          LoginManager.login(userObj, body['token']);
+        } else {
+          this.setState({ registerFailed: true, errorDetail: body['detail'] });
+        }
+      } else {
+        this.setState({ registerFailed: true });
+      }
+    }.bind(this));
   }
 });
 
